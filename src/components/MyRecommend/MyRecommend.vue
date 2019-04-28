98904b4a510114eb9c88dc98fb8c01403fc53d51
gaoyoujian <gaoyoujian@babytree-inc.com>
2019-04-28 15:35:17 +0800

    delete content

--- src/components/MyRecommend/MyRecommend.vue
+++ src/components/MyRecommend/MyRecommend.vue
@@ -2,61 +2,15 @@
 
 <template>
   <div class="my-recommend" ref="recommendRef">
-    <!-- better-scroll 滚动组件，当请求到 lists 时才渲染 -->
-    <my-scroll ref="scroll" :data="lists" class="recommend-content">
-      <div>
-        <!-- 轮播图，当请求到 recommends 时才渲染 -->
-        <div v-if="recommends.length" class="slide-wrapper">
-          <my-slider>
-            <div v-for="recommend in recommends">
-              <a :href="recommend.linkUrl">
-                <img @load="loadImg" :src="recommend.picUrl" class="needsclick">
-              </a>
-            </div>
-          </my-slider>
-        </div>
-
-        <!-- 歌单推荐列表 -->
-        <div class="recommend-list">
-          <h1 class="list-title">热门歌单推荐</h1>
-          <ul>
-            <li @click="selectItem(item)" v-for="item in lists" class="item">
-              <div class="icon">
-                <img v-lazy="item.imgurl" width="60" height="60">
-              </div>
-              <div class="text">
-                <p v-html="item.creator.name" class="name"></p>
-                <p v-html="item.dissname" class="desc"></p>
-              </div>
-            </li>
-          </ul>
-        </div>
-      </div>
-
-      <!-- loading 组件 -->
-      <div v-show="!lists.length" class="loading-container">
-        <my-loading></my-loading>
-      </div>
-    </my-scroll>
-
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-import { getRecommend, getList } from '@/api/recommend.js'
-import MySlider from '@/components/base/MySlider/MySlider'
-import MyScroll from '@/components/base/MyScroll/MyScroll'
-import MyLoading from '@/components/base/MyLoading/MyLoading'
-import { playlistMixin } from '@/common/js/mixin.js'
-import { mapMutations } from 'vuex'
+import { getRecommend } from '@/api/recommend.js'
 
 export default {
-  mixins: [playlistMixin],
   components: {
-    MySlider,
-    MyScroll,
-    MyLoading
   },
   data () {
     return {
@@ -71,40 +25,12 @@ export default {
     }, 1000)
   },
   methods: {
-    ...mapMutations({
-      setSonglist: 'SET_SONGLIST'
-    }),
-    // 子路由跳转
-    selectItem(item) {
-      this.$router.push({
-        path: `/recommend/${item.dissid}`
-      })
-
-      // 写入 vuex
-      // console.log(item)
-      this.setSonglist(item)
-    },
-    // 当有迷你播放器时，调整滚动底部距离
-    handlePlaylist(playlist) {
-      let bottom = playlist.length > 0 ? '60px' : ''
-      this.$refs.recommendRef.style.bottom = bottom
-      this.$refs.scroll.refresh()
-    },
     // 获取轮播图数据
     _getRecommend () {
       getRecommend().then((res) => {
         if (res.code === 0) {
           // console.log(res)
           this.recommends = res.data.slider
-        }
-      })
-    },
-    // 获取歌单列表数据
-    _getList () {
-      getList().then((res) => {
-        if (res.code === 0) {
-          // console.log(res)
-          this.lists = res.data.list
         }
       })
     },
